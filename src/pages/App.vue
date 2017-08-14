a2acdc2d012e35a432f0c09ad72d74fe094b9460
guowf <380482989@qq.com>
2017-08-14 16:22:44 +0800

    reg

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="wrapper">
         <div class="toast" v-show="toastMsg">{{ toastMsg }}</div>
         <router-view v-if="ready||1"></router-view>
     </div>
@@ -58,72 +58,306 @@
 </script>
 <style>
     @charset "utf-8";
-
-
-
-    /* CSS Document */
-    body, div, dl, dt, dd, ul, ol, li, h1, h2, h3, h4, h5, h6, pre, code, form, fieldset, legend, input, button, textarea, p, blockquote, th, td img{
+    body,
+    p,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    ul,
+    ol,
+    li,
+    dl,
+    dt,
+    dd,
+    table,
+    th,
+    td,
+    form,
+    fieldset,
+    legend,
+    input,
+    textarea,
+    button,
+    select {
         margin: 0;
         padding: 0;
-        border: 0;
-    }
-    * {
-        font-family:"Microsoft YaHei", "微软雅黑", '\65B0\5B8B\4F53', '\5B8B\4F53', Verdana;
-    }
-    fieldset, img {
-        border: 0;
-    }
-    abbr, acronym {
-        border: 0;
-        font-variant: normal;
-    }
-    a {
-        color:#fff;
-    }
-    body {
-        line-height: 1.5;
-        font-size: 14px;
-    }
-    ol, ul {
-        list-style: none;
-    }
-    table {
-        border-collapse: collapse;
-        border-spacing: 0;
-    }
-    caption, th {
-        text-align: left;
-    }
-    sup, sub {
-        font-size: 100%;
-        vertical-align: baseline;
-    }
-    blockquote, q {
-        quotes: none;
-    }
-    blockquote:before, blockquote:after, q:before, q:after {
-        content: '';
-        content: none;
-    }
-    :link, :visited {
-        text-decoration: none;
-    }
-    :focus {
-        outline: 0;
-    }
-    body {
-        width: 100%;
-        height: 100%;
-        margin:0 auto;
-    }
-    /*清除浮动*/
-    .clearfix:after {content:""; display:block; clear:both;}
-    .clearfix {zoom:1;}
-    .opacity30 {
-        filter:alpha(opacity=30);
-        -moz-opacity:0.3;
-        -khtml-opacity:0.3;
-        opacity: 0.3;
     }
 
+    body,
+    input,
+    textarea,
+    select,
+    button,
+    table {
+        font-size: 14px;
+        line-height: 1.25em;
+    }
+
+    html {
+        height: 100%;
+    }
+
+    body {
+        min-width: 750px;
+        font-size: 14px;
+        font-family: 'microsoft yahei', Verdana, Arial, Helvetica, sans-serif;
+        color: #333;
+        -webkit-text-size-adjust: none;
+        -webkit-user-select: none;
+        -webkit-tap-highlight-color: transparent;
+        height: 100%;
+    }
+
+    table {
+        border-collapse: collapse;
+        border-spacing: 0
+    }
+
+    fieldset,
+    img,
+    button {
+        border: 0;
+    }
+
+    li {
+        list-style: none
+    }
+
+    a {
+        text-decoration: none;
+        color: #333;
+        outline: none
+    }
+
+    img {
+        vertical-align: middle
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    input,
+    textarea,
+    select,
+    button {
+        font-size: 100%
+    }
+
+    ::-webkit-input-placeholder {
+        color: #bbb;
+    }
+
+    input[type="text"], input[type="number"], input[type="password"] {
+        -webkit-appearance: none;
+        border-radius: 0;
+    }
+
+    em,
+    i {
+        font-style: normal
+    }
+    .btn{
+        display: block;
+        text-align: center;
+    }
+    .font0{
+        font-size:0;
+        text-indent:-99999px;
+    }
+    .hide {
+        display: none
+    }
+    .toast {
+        line-height: 44px;
+        width: 580px;
+        padding: 30px 40px;
+        background: rgba(0, 0, 0, .9);
+        color: #fff;
+        font-size: 32px;
+        border-radius: 8px;
+        position: fixed;
+        left: 50%;
+        top: 40%;
+        z-index: 99;
+        transform: translate(-50%, -50%);
+        text-align: center;
+    }
+    .wrapper {
+        position: relative;
+        width:100%;
+        height:100%;
+        overflow: hidden;
+    }
+    /*头部开始*/
+    .top{
+        position: relative;
+        background: #333;
+        line-height:100px;
+    }
+    .back{
+        position: absolute;
+        left:20px;
+        top:25px;
+        width:31px;
+        height:51px;
+        background-image: url("~/images/icon-sp.png");
+        background-size: 178px 51px;
+        background-position: 0 0;
+    }
+    .login{
+        position: absolute;
+        right:35px;
+        top:0;
+        font-size:30px;
+        color: #fff;
+    }
+    .top h1{
+        text-align: center;
+        font-size:36px;
+        color: #fff;
+        font-weight:normal;
+    }
+    /*头部结束*/
+    .form{
+        padding:0 40px;
+    }
+    .form .prompt{
+
+    }
+    .form h2{
+        font-size:36px;
+        color: #020202;
+        font-weight:normal;
+        line-height:48px;
+    }
+    .unable{
+        color: #c0bfc4 !important;
+        border-color: #c0bfc4 !important;
+    }
+    .use-msg{
+        margin-top:30px;
+    }
+    .rebate-msg{
+        margin-top:54px;
+    }
+    .reg-input{
+        position: relative;
+        width:100%;
+        height:78px;
+        overflow: hidden;
+        line-height:75px;
+        border-bottom:1px solid #ededed;
+    }
+    .prompt{
+        float: left;
+        color: #f64176;
+        font-size:28px;
+    }
+    .tips{
+        float: left;
+        margin-left:15px;
+        line-height:78px;
+        font-size:28px;
+        color: #c0bfc4;
+    }
+    .reg-input input{
+        position: absolute;
+        z-index:1;
+        left:0;
+        top:0;
+        width:100%;
+        height:100%;
+        line-height:78px;
+        font-size:28px;
+        color: #000;
+        outline:none;
+        border:none;
+        background: transparent;
+        text-indent:30px;
+    }
+    .seng-code{
+        position: absolute;
+        z-index:2;
+        right:8px;
+        top:6px;
+        width:252px;
+        height:66px;
+        overflow: hidden;
+        border:1px solid #6569c6;
+        -webkit-border-radius: 6px;
+        -moz-border-radius: 6px;
+        border-radius: 6px;
+        font-size:28px;
+        color: #6569c6;
+        line-height:66px;
+    }
+    .eye{
+        position: absolute;
+        z-index:2;
+        top:22px;
+        right:30px;
+        width:61px;
+        height:35px;
+        overflow: hidden;
+        background-image: url("../images/icon-sp.png");
+        background-size: 178px 51px;
+        background-repeat: no-repeat;
+    }
+    .eye-on{
+        background-position:-65px 0;
+    }
+    .eye-off{
+        background-position: -116px 0;
+    }
+    .protocol{
+        margin: 24px 0 66px 0;
+        line-height:24px;
+        font-size:24px;
+        color: #808080;
+    }
+    .protocol input{
+        width:23px;
+        height:23px;
+        overflow: hidden;
+        background: #f0f0f0;
+        border:1px solid #ddd;
+    }
+    .protocol a{
+        color: #6569c6;
+    }
+    .reg-form input[type="submit"]{
+        display: block;
+        width:100%;
+        line-height:78px;
+        text-align: center;
+        background: #6569c6;
+        -webkit-border-radius: 6px;
+        -moz-border-radius: 6px;
+        border-radius: 6px;
+        font-size:36px;
+        color: #fff;
+        border:none;
+        outline:none;
+    }
+    .check-time{
+        text-align: center;
+        font-size:28px;
+        color: #808080;
+        line-height:60px;
+    }
+    .contact-us{
+        position: absolute;
+        left:0;
+        bottom:50px;
+        width:100%;
+        text-align: center;
+        font-size:24px;
+        color: #808080;
+    }
 </style>
--- src/pages/dailiDataPage/Regis.vue
+++ src/pages/dailiDataPage/Regis.vue
@@ -1,9 +1,52 @@
 <template>
     <div>
-        <p>啦啦啦阿拉fsdfdsfsdf拉阿里</p>
-        <input type="tel" placeholder="11位手机号，限中国大陆" maxlength="11" @blur="checkTel">
-        <input type="password" placeholder="设置登录密码，6~12位" id="passDom">
-        <button  @click="changeType" >点击显示密码</button>
+        <div class="main">
+            <div class="form reg-form">
+                <h2 class="use-msg">账号信息</h2>
+                <div class="reg-input reg-phone">
+                    <span class="prompt">*</span>
+                    <span class="tips">11位手机号码，限中国大陆</span>
+                    <input type="text" name="phone" required>
+                </div>
+                <div class="reg-input reg-check">
+                    <span class="prompt">*</span>
+                    <span class="tips">4位验证码</span>
+                    <input type="text" name="check" required>
+                    <a href="javascript:;" class="btn seng-code unable">发送验证码</a>
+                </div>
+                <div class="reg-input reg-pasw">
+                    <span class="prompt">*</span>
+                    <span class="tips">设置登录密码</span>
+                    <input type="text" name="password" required>
+                    <a href="javascript:;" class="btn eye eye-on"></a>
+                </div>
+                <h2 class="rebate-msg">返佣信息</h2>
+                <div class="reg-input reg-name">
+                    <span class="prompt">*</span>
+                    <span class="tips">真实姓名</span>
+                    <input type="text" name="name" required>
+                </div>
+                <div class="reg-input reg-alipay">
+                    <span class="prompt">*</span>
+                    <span class="tips">支付宝账号，不可修改</span>
+                    <input type="text" name="alipay" required>
+                </div>
+                <div class="reg-input reg-qq">
+                    <span class="prompt">*</span>
+                    <span class="tips">QQ</span>
+                    <input type="text" name="qq" required>
+                </div>
+                <div class="protocol">
+                    <input type="checkbox">
+                    <span>18岁以上，且同意</span>
+                    <a href="protocol.html">《代理合作协议》</a>
+                </div>
+                <input type="submit" value="提交">
+                <p class="check-time">3个工作日内完成审核</p>
+                <p class="contact-us">如需帮助请联系客服QQ : 3157085145</p>
+            </div>
+        </div>
+        <div class="toast hide">ID输入错误，请重新输入！</div>
     </div>
 </template>
 
@@ -44,10 +87,4 @@
     }
 </script>
 <style>
-    input{
-        border: 1px solid #000;
-    }
-    p{
-        font-size: 0.42rem;
-    }
 </style>
