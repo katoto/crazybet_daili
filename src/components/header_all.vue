b83caedb7bb98b5d7d870a903667d5f1e395e641
xiezg <xiezg@500wan.com>
2017-08-17 15:52:23 +0800

    'dd'

--- src/components/header_all.vue
+++ src/components/header_all.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="top">
         <a href="javascript:;" class="btn font0 back"  v-tap="{ methods:goBackFn, params : 'back' }">返回</a>
-        <a href="#" class="btn login" :class="{'user':showUserIcon ,'home':showHomeIcon ,'payList':showPayListIcon}" v-tap="{ methods:jumpToPage }">{{ rightTitle }}</a>
+        <a href="javascript:;" class="btn login" :class="{'user':showUserIcon ,'home':showHomeIcon ,'payList':showPayListIcon ,'date-rebate':showRebate}" v-tap="{ methods:jumpToPage }">{{ rightTitle }}</a>
         <h1>{{ personTitle }}</h1>
     </div>
 </template>
@@ -14,6 +14,7 @@
                 showUserIcon:false,
                 showHomeIcon:false,
                 showPayListIcon:false,
+                showRebate:false
             }
         },
         methods: {
@@ -21,10 +22,10 @@
                 switch (params ) {
                 case 'login':
                     this.$router.push(`/my/betlist`);
-                    break
+                    break;
                 case 'regis':
                     this.$router.push(`/my/charge`);
-                    break
+                    break;
                 case 'goLand':
                     /*  */
                     alert('跳转落地页');
@@ -51,6 +52,10 @@
                     case 'myHomeApplyList':
                         this.$router.push(`/myHomeApplyList`);
                         break;
+                    case 'dateRebate':
+                        /* 显示日历 */
+                        this.$store.commit('setCalendar',new Date().getTime());
+                        break;
                     default:window.history.back() ;
                 }
             },
@@ -60,7 +65,9 @@
 
         },
         computed: {
-
+            showCalendar(){
+                return this.$store.state.myHomeObj.showCalendar
+            }
         },
         mounted () {
             if( this.iconStyle === 'login' ){
@@ -73,6 +80,8 @@
                 this.showHomeIcon = true;
             }else if( this.iconStyle === 'myHomeApplyList' ){
                 this.showPayListIcon = true;
+            } else if( this.iconStyle === 'dateRebate'){
+                this.showRebate = true;
             }
         },
         filters: {
--- src/pages/dailiDataPage/myhomeRebate.vue
+++ src/pages/dailiDataPage/myhomeRebate.vue
@@ -1,10 +1,6 @@
 <template>
     <div class="rebate">
-        <div class="top">
-            <a href="javascript:;" class="btn font0 back">返回</a>
-            <a href="#" class="btn date-rebate">日期选择</a>
-            <h1>返佣详情</h1>
-        </div>
+        <Header_all person-title="返佣详情" :icon-style="'dateRebate'"></Header_all>
         <div class="rebate-msg">
             <ul>
                 <li class="rebate-msg1">
@@ -21,7 +17,7 @@
             <div class="view-t">
                 <ul>
                     <li>用户</li>
-                    <li>投注</li>
+                    <li>充值</li>
                     <li>盈亏</li>
                     <li>手续费</li>
                     <li>奖品成本</li>
@@ -221,21 +217,107 @@
                 </ul>
             </div>
         </div>
+        <div class="page-datetime">
+            <mt-datetime-picker
+                    ref="picker"
+                    type="date"
+                    v-model="valueTime"
+                    v-bind:startDate=pickerValue
+                    v-bind:endDate=endDate
+                    year-format="{value} 年"
+                    month-format="{value} 月"
+                    date-format="{value} 日"
+                    @confirm="handleChange">
+            </mt-datetime-picker>
+
+        </div>
     </div>
 </template>
 
 <script>
+    import Header_all from '~components/header_all.vue'
     export default {
         data(){
             return {
-                title: ''
+                title: '',
+                valueTime: '2016-12-11',
+                pickerValue:new Date(new Date().getFullYear() - 1, 0, 1),
+                endDate:new Date(),
             }
         },
-        watch: {},
-        methods: {},
-        computed: {},
+        watch: {
+            showCalendar(){
+                this.open('picker')
+            }
+        },
+        methods: {
+            open(picker) {
+                this.$refs[picker].open();
+            },
+            handleChange(value) {
+                this.valueTime = this.matchTimeThunder(value);
+            },
+            matchTimeThunder (time, format = 'yyyy-MM-dd') {
+                let t = new Date(time);
+                let tf = function (i) {
+                    return (i < 10 ? '0' : '') + i
+                };
+                return format.replace(/year|yyyy|MM|dd|HH|mm|ss/g, function (a) {
+                    switch (a) {
+                        case 'year':
+                            return tf(t.getFullYear()).slice(2);
+                        case 'yyyy':
+                            return tf(t.getFullYear());
+                        case 'MM':
+                            return tf(t.getMonth() + 1);
+                        case 'mm':
+                            return tf(t.getMinutes());
+                        case 'dd':
+                            return tf(t.getDate());
+                        case 'HH':
+                            return tf(t.getHours());
+                        case 'ss':
+                            return tf(t.getSeconds())
+                    }
+                })
+            }
+        },
+        computed: {
+            showCalendar(){
+                return this.$store.state.myHomeObj.showCalendar
+            }
+        },
+        components:{
+            Header_all
+        },
         mounted(){
-
+            this.valueTime = this.matchTimeThunder(new Date());
+        },
+        filters:{
+            matchTimeThunder (time, format = 'yyyy-MM-dd') {
+                let t = new Date(+time * 1000)
+                let tf = function (i) {
+                    return (i < 10 ? '0' : '') + i
+                }
+                return format.replace(/year|yyyy|MM|dd|HH|mm|ss/g, function (a) {
+                    switch (a) {
+                        case 'year':
+                            return tf(t.getFullYear()).slice(2)
+                        case 'yyyy':
+                            return tf(t.getFullYear())
+                        case 'MM':
+                            return tf(t.getMonth() + 1)
+                        case 'mm':
+                            return tf(t.getMinutes())
+                        case 'dd':
+                            return tf(t.getDate())
+                        case 'HH':
+                            return tf(t.getHours())
+                        case 'ss':
+                            return tf(t.getSeconds())
+                    }
+                })
+            }
         }
     }
 </script>
